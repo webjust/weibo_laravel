65ef5913360d91493b3879ed1ac1ea3815e4544c
webjust <604854119@qq.com>
2017-06-20 14:21:17 +0800

    gulp css

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,15 @@
+<div class="col-md-12">
+  <footer class="footer">
+    <small class="slogon">
+      <img class="brand-icon" src="">
+      <a href="">
+        优帆远扬 | 创造不息，交付不止
+      </a>
+    </small>
+    <nav>
+      <ul>
+        <li><a href="/about">关于</a></li>
+      </ul>
+    </nav>
+  </footer>
+</div>
